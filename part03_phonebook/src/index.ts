474c2ce4b3272a31adc063347f6ba944d79917d9
jaimetrovoada <jaimetrovoada@gmail.com>
2023-01-25 00:20:04 +0800

    finish exercise 3.8

--- part03_phonebook/src/index.ts
+++ part03_phonebook/src/index.ts
@@ -1,11 +1,38 @@
 import express from "express";
 import personsRouter, { data } from "./routes/api/persons";
-import morgan from "morgan"
+import morgan from "morgan";
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'))
+
+morgan.token("request-body", function (req, res) {
+  if (req.method === "POST") {
+    return JSON.stringify((req as any).body);
+  }
+});
+
+app.use(
+  morgan(
+    function (tokens, req, res) {
+      return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, "content-length"),
+        "-",
+        tokens["response-time"](req, res),
+        "ms",
+        tokens["request-body"](req, res),
+      ].join(" ");
+    }
+    /* {
+      skip: function (req, res) {
+        return req.method !== "POST";
+      },
+    } */
+  )
+);
 
 app.use("/api/persons", personsRouter);
 
