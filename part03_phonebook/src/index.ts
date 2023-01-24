5754ee59f1c0d000d74cd375c9d43f6b0e36e2fa
jaimetrovoada <jaimetrovoada@gmail.com>
2023-01-24 22:54:04 +0800

    finish exercise 3.2

--- part03_phonebook/src/index.ts
+++ part03_phonebook/src/index.ts
@@ -1,9 +1,17 @@
 import express from "express";
-import personsRouter from "./routes/api/persons";
+import personsRouter, { data } from "./routes/api/persons";
 
 const app = express();
 
 app.use("/api/persons", personsRouter);
+
+app.get("/info", (req, res) => {
+  const numOfEntries = data.length;
+  const date = new Date().toString();
+  res.send(
+    `<p>Phonebook has info for ${numOfEntries} people.</p><p>${date}</p>`
+  );
+});
 
 app.listen(process.env.PORT || 3001, () =>
   console.log(`server started on http://localhost:${process.env.PORT || 3001}`)
