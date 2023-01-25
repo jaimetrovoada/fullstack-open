b63aca7bdec11c7af9f028763cc0c0a6c26efa07
jaimetrovoada <jaimetrovoada@gmail.com>
2023-01-25 23:28:41 +0800

    finish exercise 2.20

--- part02/countries/src/components/CountryInfo.tsx
+++ part02/countries/src/components/CountryInfo.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const CountryInfo = ({
   name,
@@ -13,6 +14,19 @@ const CountryInfo = ({
   languages: string[];
   flag: string;
 }) => {
+  const [weather, setWeather] = useState<Record<any, any>>();
+  const apiKey = process.env.REACT_APP_API_KEY;
+  console.log({ apiKey });
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}&units=metric`
+      )
+      .then((res) => setWeather(res.data));
+  }, [apiKey, capital]);
+
+  console.log({ weather, api: process.env.REACT_APP_API_KEY });
   return (
     <>
       <h2>{name}</h2>
@@ -25,6 +39,16 @@ const CountryInfo = ({
         ))}
       </ul>
       <img src={flag} alt="" />
+
+      <h3>Weather in {capital}</h3>
+      <div>
+        <p>temperature {weather?.main.temp} Celcius</p>
+        <img
+          src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}
+          alt=""
+        />
+        <p>wind {weather?.wind.speed} m/s</p>
+      </div>
     </>
   );
 };
