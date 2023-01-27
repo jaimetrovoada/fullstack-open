1694c8fb269755881db5573c5cc878355f5077bc
jaimetrovoada <jaimetrovoada@gmail.com>
2023-01-27 23:18:46 +0800

    finish exercise 4.5

--- part04/bloglist/utils/list_helper.ts
+++ part04/bloglist/utils/list_helper.ts
@@ -10,7 +10,23 @@ const totalLikes = (blogs: Record<any, any>[]) => {
 	return sum
 }
 
+const favoriteBlog = (blogs: Record<any, any>[]) => {
+	if (blogs.length === 0 || !blogs) {
+		return null
+	}
+    
+	const fav = blogs.reduce((mostLiked, blog) => {
+		if (blog.likes > mostLiked.likes) {
+			return blog
+		}
+		return mostLiked
+	}, { likes: 0 })
+    
+	return fav
+}
+
 export default {
 	dummy,
 	totalLikes,
+	favoriteBlog
 } as const
