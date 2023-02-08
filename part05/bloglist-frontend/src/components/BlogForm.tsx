9bbe100395e26cf75bc532cd1201bd7ada04c32e
jaimetrovoada <jaimetrovoada@gmail.com>
2023-02-08 20:12:57 +0800

    finish exercise 5.5

--- part05/bloglist-frontend/src/components/BlogForm.tsx
+++ part05/bloglist-frontend/src/components/BlogForm.tsx
@@ -9,6 +9,8 @@ const BlogForm = ({setBlogs, setMessage}: {setBlogs: React.Dispatch<any>, setMes
     const [author, setAuthor] = useState<string>('')
     const [url, setUrl] = useState<string>('')
 
+    const [isVisible, setIsVisible] = useState<boolean>(false)
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         
@@ -28,13 +30,21 @@ const BlogForm = ({setBlogs, setMessage}: {setBlogs: React.Dispatch<any>, setMes
             }, 5000)    
         }
     }
+
+    if (!isVisible) {
+        return <>
+        <button onClick={() => setIsVisible(true)}>new note</button></>
+    }
   return (
+    <>
       <form onSubmit={handleSubmit}>
           <div>title:<input type="text" name="title" id="title" onChange={({target}) => setTitle(target.value)} /></div>
           <div>author:<input type="text" name="author" id="author" onChange={({target})=> setAuthor(target.value)}/></div>
           <div>url:<input type="url" name="url" id="url" onChange={({target})=> setUrl(target.value)}/></div>
     <button type="submit">create</button>
     </form>
+    <button onClick={()=> setIsVisible(false)}>cancel</button>
+    </>
   )
 }
 
